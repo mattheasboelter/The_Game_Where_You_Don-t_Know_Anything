18c2fe098ac5c7daa3d5f5731cf675142ce2cefc
TARDIS-Maker <tardismodeler@gmail.com>
2016-06-07 16:57:30 -0400

    Basic jumping. You can only jump when you're on the ground. Character now has momentum, so when you move and then jump, it's preserved

--- Godottesting/testscenefolder/Scripts/basicmove.gd
+++ Godottesting/testscenefolder/Scripts/basicmove.gd
@@ -1,25 +1,65 @@
-
 extends RigidBody2D
 
-# member variables here, example:
-# var a=2
-# var b="textvar"
+var self_collision
+
+var is_moving_left = false
+var is_moving_right = false
+var is_jumping = false
+
+var jump_timer = 0;
+
+var speed = 70
+var jump_height = 150
 
 func _ready():
+	# set_process(true)
 	set_fixed_process(true)
-	
-	
+	set_process_input(true)
+
+	self_collision = get_node('player_collision_sensor')
+	self_collision.add_exception(self)
+
+func _input(event):
+	# Handle Keypress Events
+	if(event.is_pressed()):
+		# Handle MOVE LEFT Event
+		if(event.is_action('MOVE_LEFT') and not event.is_echo()):
+			is_moving_left = true
+		# Handle MOVE RIGHT Event
+		if(event.is_action('MOVE_RIGHT') and not event.is_echo()):
+			is_moving_right = true
+		# Handle JUMP Event
+		if(event.is_action('JUMP') and not event.is_echo()):
+			if(self_collision.is_colliding()):
+				Jump()
+
+	# Handle Key Release Events
+	else:
+		# Handle MOVE LEFT Event
+		if(event.is_action('MOVE_LEFT') and not event.is_echo()):
+			is_moving_left = false
+		# Handle MOVE RIGHT Event
+		if(event.is_action('MOVE_RIGHT') and not event.is_echo()):
+			is_moving_right = false
+
+
 func _fixed_process(delta):
-	#Just some variables
-	var ownX = get_pos().x
-	var ownY = get_pos().y
-	var speed = 1 #this can change if a speed boost is found
-	
-	#move left
-	if(Input.is_key_pressed(KEY_A)):
-		set_pos(Vector2(ownX-speed, ownY))
-	
-	#move right
-	if(Input.is_key_pressed(KEY_D)):
-		set_pos(Vector2(ownX+speed, ownY))
-		
+	var x = get_pos().x
+	var y = get_pos().y
+
+	# move left
+	if(is_moving_left):
+		if(self_collision.is_colliding()):
+			set_axis_velocity(Vector2(-speed, 0))
+
+	# move right
+	if(is_moving_right):
+		if(self_collision.is_colliding()):
+			set_axis_velocity(Vector2(speed, 0))
+
+
+func Jump():
+	set_axis_velocity(Vector2(0, -jump_height))
+
+	# is_moving_left = false
+	# is_moving_right = false
