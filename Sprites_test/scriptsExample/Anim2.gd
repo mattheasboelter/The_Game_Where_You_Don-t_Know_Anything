8b79eefd3d7b19080336bf7b134b07a7bc24640d
TARDIS-Maker <tardismodeler@gmail.com>
2016-06-08 17:27:05 -0400

    Make the robot walk when pressing W

--- Sprites_test/scriptsExample/Anim2.gd
+++ Sprites_test/scriptsExample/Anim2.gd
@@ -1,5 +1,17 @@
 
 extends AnimatedSprite
 
+var animation_player
+
 func _ready():
-	get_node("AnimationPlayer").play("RobotWalkCycle")
+	#set_fixed_process(true)
+	set_process_input(true)
+	
+	animation_player = get_node("AnimationPlayer")
+	
+func _input(event):
+	if(event.is_pressed()):
+		# Handle MOVE LEFT Event
+		if(event.is_action('WALK') and not event.is_echo()):
+			animation_player.play("RobotWalkCycle")
+	
