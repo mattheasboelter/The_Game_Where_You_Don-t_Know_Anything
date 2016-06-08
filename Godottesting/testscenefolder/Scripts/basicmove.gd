34bdc5a73d1bd729bf57200d399c10e19aae1695
TARDIS-Maker <tardismodeler@gmail.com>
2016-06-07 22:15:46 -0400

    Made the raycast2D object rotate with gravity changes

--- Godottesting/testscenefolder/Scripts/basicmove.gd
+++ Godottesting/testscenefolder/Scripts/basicmove.gd
@@ -1,5 +1,11 @@
 extends RigidBody2D
 
+# Constants for Gravity Direction
+const DOWN = Vector2(0, 1)
+const UP = Vector2(0, -1)
+const RIGHT = Vector2(1, 0)
+const LEFT = Vector2(-1, 0)
+
 var self_collision
 
 var is_moving_left = false
@@ -39,16 +45,16 @@ func _input(event):
 		### Handle Gravity changes
 		# Down
 		if(event.is_action('GRAVITY_DOWN') and not event.is_echo()):
-			ChangeGravityDirection(0, 1)
+			ChangeGravityDirection(DOWN)
 		# Up
 		if(event.is_action('GRAVITY_UP') and not event.is_echo()):
-			ChangeGravityDirection(0, -1)
+			ChangeGravityDirection(UP)
 		# Right
 		if(event.is_action('GRAVITY_RIGHT') and not event.is_echo()):
-			ChangeGravityDirection(1, 0)
+			ChangeGravityDirection(RIGHT)
 		# Left
 		if(event.is_action('GRAVITY_LEFT') and not event.is_echo()):
-			ChangeGravityDirection(-1, 0)
+			ChangeGravityDirection(LEFT)
 
 	# Handle Key Release Events
 	else:
@@ -78,5 +84,18 @@ func _fixed_process(delta):
 func Jump():
 	set_axis_velocity(Vector2(0, -jump_height))
 
-func ChangeGravityDirection(x, y):
-	Physics2DServer.area_set_param(get_world_2d().get_space(), Physics2DServer.AREA_PARAM_GRAVITY_VECTOR, Vector2(x, y)) #Change Gravity Direction
+func ChangeGravityDirection(direction):
+	var rotation
+
+	if(direction == Vector2(0, 1)):
+		rotation = 0
+	elif(direction == Vector2(0, -1)):
+		rotation = 180
+	elif(direction == Vector2(1, 0)):
+		rotation = -90
+	elif(direction == Vector2(-1, 0)):
+		rotation = 90
+
+	self_collision.set_rot(rotation)
+
+	Physics2DServer.area_set_param(get_world_2d().get_space(), Physics2DServer.AREA_PARAM_GRAVITY_VECTOR, direction) #Change Gravity Direction
