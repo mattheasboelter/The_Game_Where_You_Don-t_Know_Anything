0672c218e43f3de8221677fb819189d0deaa6ab4
Vincent <jww_squarepapers@q.comgit config --global user.email jww_squarepapers@q.comgit config --global user.name VScalia>
2016-06-08 16:41:37 -0600

    sprite half-integrated into game

---
+++ Godottesting/scriptsExample/Anim.gd
@@ -0,0 +1,9 @@
+
+extends AnimationPlayer
+
+
+func _ready():
+	set_fixed_process(true)
+	
+func _process(delta):
+	AnimationPlayer.
---
+++ Godottesting/scriptsExample/Anim2.gd
@@ -0,0 +1,20 @@
+
+extends AnimatedSprite
+
+var animation_player
+
+func _ready():
+	#set_fixed_process(true)
+	set_process_input(true)
+	
+	animation_player = get_node("AnimationPlayer")
+	
+func _input(event):
+	if(event.is_pressed()):
+		# Handle MOVE LEFT Event
+		if(event.is_action('MOVE_LEFT')or event.is_action('MOVE_RIGHT') and not event.is_echo()):
+			animation_player.play("RobotWalkCycle")
+		if(Input.is_key_pressed(KEY_A)):
+			get_node(".").set_flip_h(true)
+		if(Input.is_key_pressed(KEY_D)):
+			get_node(".").set_flip_h(false)
