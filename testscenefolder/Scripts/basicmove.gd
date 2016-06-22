95347a71fe8883c236418e65e047d5e27803fd32
Hugo Sales <hdcs333@gmail.com>
2016-06-22 19:08:11 +0100

    Refactor and Landing fix

--- testscenefolder/Scripts/basicmove.gd
+++ testscenefolder/Scripts/basicmove.gd
@@ -2,6 +2,24 @@ extends RigidBody2D
 
 # Constants for Gravity Direction
 
+class PlayerState:
+	
+	const move_directions = {
+		'NONE' : 0,
+		'LEFT' : 1,
+		'RIGHT': 2
+	}
+	var move_dir = move_directions.NONE
+	
+	const jumps = {
+		'NOT_JUMPING' : 0,
+		'JUMPING'     : 1,
+		'JUMP_ECHO'   : 2, #Wether you were jumping before
+	}
+	var jumping
+	var jump_frame
+	
+
 const directions = {
 	'DOWN' : Vector2(0, 1),
 	'UP'   : Vector2(0, -1),
@@ -15,7 +33,7 @@ const states = {
 	'MOVING_LEFT' : 2,
 	'MOVING_RIGHT': 4
 }
-var player_state = 0 #Current Character state. C style bit flag
+var player_state #Current Character state
 
 const traits = {
 	'SPEED'      : 70,
@@ -26,13 +44,14 @@ var self_collision
 var animation_player
 var sprite
 
-var jump_timer = 0
+#var jump_timer = 0
 var jumpframe
 
 var gravity_change = false
 var gravity_direction
 
 func _ready():
+	player_state = PlayerState.new()
 	gravity_direction = 'DOWN'
 
 	set_fixed_process(true)
@@ -43,8 +62,8 @@ func _ready():
 
 	animation_player = get_node("AnimatedSprite/AnimationPlayer")
 	sprite = get_node("AnimatedSprite")
-
-	player_state = states.STANDING
+	
+	player_state.jump_frame = get_tree().get_frame()
 	animation_player.play("Robot_stand")
 
 func _input(event):
@@ -52,19 +71,19 @@ func _input(event):
 	if(event.is_pressed()):
 		for key in ['LEFT', 'RIGHT']:
 			if(event.is_action('MOVE_' + key) and not event.is_echo()):
-				player_state = states['MOVING_' + key] | (player_state & states.JUMPING)
+				player_state.move_dir += player_state.move_directions[key] #Avoid prefering RIGHT if both are pressed
 				animation_player.play("Robot_walk")
-				sprite.set_flip_h(states['MOVING_' + key] & states.MOVING_LEFT != 0)
-
+				#sprite.set_flip_h(states['MOVING_' + key] & states.MOVING_LEFT != 0)
+				if(gravity_direction == 'DOWN' || gravity_direction == 'LEFT'):
+					sprite.set_flip_h(key == 'LEFT')
 				if(gravity_direction == 'UP' || gravity_direction == 'RIGHT'):
-					sprite.set_flip_h(!(states['MOVING_' + key] & states.MOVING_LEFT != 0))
+					sprite.set_flip_h(key != 'LEFT')
 
 
 		if(event.is_action('JUMP') and not event.is_echo()):
 			if(self_collision.is_colliding()):
-				player_state |= states.JUMPING  #Set Jumping
-				player_state &= ~states.STANDING #Set Not Standing
-				Jump()
+				player_state.jumping = player_state.jumps.JUMPING
+				#Jump()
 
 		# Handle Gravity changes
 		for key in directions.keys():
@@ -76,7 +95,7 @@ func _input(event):
 	else:
 		for key in ['LEFT', 'RIGHT']:
 			if(event.is_action('MOVE_' + key) and not event.is_echo()):
-				player_state = states.STANDING
+				player_state.move_dir = player_state.move_directions.NONE
 				animation_player.play("Robot_stand")
 
 func _fixed_process(delta):
@@ -91,24 +110,32 @@ func _fixed_process(delta):
 		self.set_rot(deg2rad(rotations[gravity_direction]))
 		gravity_change = false
 
-	for key in ['MOVING_LEFT', 'MOVING_RIGHT']:
-		if((player_state & states[key]) and self_collision.is_colliding()): #Not ideal piece ahead
+	for key in ['LEFT', 'RIGHT']:
+		if((player_state.move_dir == player_state.move_directions[key]) and self_collision.is_colliding()):
+			var mult = {false : -1, true : 1}
+			var speed = traits.SPEED * mult[player_state.move_dir == player_state.move_directions.RIGHT] #((player_state.move_dir & player_state.move_directions[key]) - 3)
 			if(gravity_direction == 'LEFT' || gravity_direction == 'RIGHT'):
-				set_axis_velocity(Vector2(0, traits.SPEED * ((player_state & states[key]) - 3))) #Temporary workaround for the lack of ternary operators
+				set_axis_velocity(Vector2(0, speed))
 			else:
-				set_axis_velocity(Vector2(traits.SPEED * ((player_state & states[key]) - 3), 0)) #Temporary workaround for the lack of ternary operators
+				set_axis_velocity(Vector2(speed, 0))
 
-	if(player_state & states.JUMPING):
-		if(self_collision.is_colliding()):
-			if(get_tree().get_frame() - jumpframe > 10):
-				player_state |= states.STANDING  #Set Standing
-				player_state &= ~states.JUMPING  #Set Not Jumping
+	if(self_collision.is_colliding()):
+		if(player_state.jumping == player_state.jumps.JUMPING):
+			set_axis_velocity(-(directions[gravity_direction] * traits.JUMP_HEIGHT))
+			animation_player.play("Robot_jump")
+			player_state.jump_frame = get_tree().get_frame()
+			
+			player_state.jumping = player_state.jumps.JUMP_ECHO
+		if(get_tree().get_frame() - player_state.jump_frame > 20 && (player_state.jumping == player_state.jumps.JUMP_ECHO)):
+			player_state.jumping = player_state.jumps.NOT_JUMPING
+			
+			if(player_state.move_dir == player_state.move_directions.NONE):
 				animation_player.play("Robot_stand")
-
-func Jump():
-	set_axis_velocity(-(directions[gravity_direction] * traits.JUMP_HEIGHT))
-	animation_player.play("Robot_jump")
-	jumpframe = get_tree().get_frame()
+			elif(not player_state.move_dir == (player_state.move_directions.LEFT + player_state.move_directions.RIGHT)): #Avoid trying to walk both ways
+				animation_player.play("Robot_walk")
+				
+	if(player_state.move_dir == (player_state.move_directions.LEFT + player_state.move_directions.RIGHT) and self_collision.is_colliding()):
+		animation_player.play("Robot_stand")
 
 func ChangeGravityDirection(key):
 	gravity_change = true
