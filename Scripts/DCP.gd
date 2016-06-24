07f0336f6f7da10e8974f57fc3fabd202853560a
TARDIS-Maker <tardismodeler@gmail.com>
2016-06-24 00:28:10 -0400

    removed timer on DCP after some discussion with radish.

--- Scripts/DCP.gd
+++ Scripts/DCP.gd
@@ -7,16 +7,9 @@ var timer
 func _ready():
 	set_fixed_process(true)
 
-	timer = get_node("Timer")
 	directional_collision = get_node("RayCast2D")
 	directional_collision.add_exception(self)
 
 func _fixed_process(delta):
 	if(directional_collision.is_colliding()):
-		if(timer.get_time_left() == 0):
-			timer.start()
-
-
-
-func _on_Timer_timeout():
-	self.queue_free()
+		self.queue_free()
